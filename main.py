467975a5cb22d26d80290448c353bc121cc43f85
2280601672-phanthuky <phanthuky12@gmail.com>
2026-03-04 14:14:17 +0700

    them main

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ def div (a : int, b : int) -> int:
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a // b
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    print(f"Sum: {sum(a, b)}")
+    print(f"Subtraction: {sub(a, b)}")
+    print(f"Multiplication: {mul(a, b)}")
+    print(f"Division: {div(a, b)}")
